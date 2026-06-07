6e9161b3f80fb84f28f947a1fd9eb26dd521ec84
real-arnel-roy <mrreall1506@gmail.com>
2026-06-07 14:11:14 +0530

    first

---
+++ main.py
@@ -0,0 +1,26 @@
+while True:
+    print("---Menu---")
+    print("1. Enter URL \n2. See URLs\n3. EXIT")
+    print("----------")
+    ch = int(input("\n\nWhat do you Choose - "))
+
+    def enter_url():
+        url = input("Paste the URL - ")
+        with open("download.json", "w") as file:
+            file.write(url)
+        print("URL Downloaded")
+
+    def see_url():
+        with open("download.json", "r") as file:
+            down = file.read()
+
+        print(down)
+
+    if ch == 1:
+        enter_url()
+    elif ch == 2:
+        see_url()
+    elif ch == 3:
+        break
+    else:
+        print("Invalid Option")
